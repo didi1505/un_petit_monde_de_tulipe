dc2a6aa14be08ce1c9a684927855b3e368f0eed0
didi1505 <dina.osman1505@gmail.com>
2024-08-01 10:07:25 +0200

    Add files via upload

--- app.js
+++ app.js
@@ -1,15 +1,25 @@
-window.onload = function() {
-	alert("Bienvenue dans le petit monde de Tulipe !");
-	
-	let sport = "";
-	while(sport != "tennis") {
-		sport = prompt("Quel est le sport préféré de Stephen Malkmus, le chanteur de Pavement ?");
-	};
+setTimeout(function() {
 
-	let moina = document.createElement("a");
-	moina.href = "https://moina-v.github.io/tennis/";
-	moina.textContent = "Lien";
-	let lien = document.getElementById("malkmus");
-	let div = document.getElementById("pavement")
-	div.insertBefore(moina, lien);
-}
+    // Affiche une alerte après 5 secondes
+    alert("Bienvenue dans le petit monde de Tulipe !");
+    alert("Répondez à la question suivante pour trouver un nouvel indice :");
+    
+    let sport = "";
+
+    // Boucle jusqu'à ce que l'utilisateur saisisse "tennis"
+    while(sport.toLowerCase() != "tennis") {
+        sport = prompt("Quel sport pratique Serena Williams ?");
+    }
+
+    // Création d'un lien <a>
+    let moina = document.createElement("a");
+    moina.href = "https://moina-v.github.io/tennis/";
+    moina.textContent = "Visitez le site de Moina";
+    alert("Trouve le lien qui vient d'apparaître dans le site !")
+    
+    // Insertion du lien dans la page
+    let lien = document.getElementById("malkmus");
+    let div = document.getElementById("pavement");
+    div.insertBefore(moina, lien);
+
+}, 10000);
